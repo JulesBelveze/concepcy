7b671794d78371bb3a57afe3ab5204d3f9e5c0e5
JulesBelveze <jules.belveze@hotmail.fr>
2022-07-24 17:03:07 +0200

    chore: use request_boost for inference speed up

--- concepcy/conceptnet_linker.py
+++ concepcy/conceptnet_linker.py
@@ -1,7 +1,8 @@
 import os
+from collections import ChainMap, defaultdict
 from typing import List, Dict
 
-import requests
+from request_boost import boosted_requests
 from spacy.language import Language
 from spacy.tokens import Doc, Token
 
@@ -33,39 +34,56 @@ class ConcepCyComponent:
         Doc.set_extension("concepts", default={})
         Token.set_extension("concepts", default={})
 
-    def _make_request(self, word: str) -> Dict[str, List[Dict]]:
+    def _make_requests(self, words: List[str]) -> List[Dict]:
         """"""
-        response = requests.get(os.path.join(self.url, word))
-        content = response.json()
+        urls = [os.path.join(self.url, word) for word in words]
+        responses = boosted_requests(urls=urls, no_workers=32, max_tries=5, timeout=5, headers=None, parse_json=True)
+        return responses
 
-        enrichments = {rel: [] for rel in self.relations_of_interest}
-        for edge in content["edges"]:
+    def _parse_response(self, response: Dict) -> Dict[str, Dict[str, List]]:
+        """"""
+        word = response["@id"].split("/")[-1]  # retrieving word
+
+        enrichments = defaultdict(list)
+        for edge in response["edges"]:
             relation = edge["@id"].split("/")[4]
             if relation in self.relations_of_interest:
                 enrichments[relation].append({"weight": edge["weight"], "text": edge["surfaceText"]})
-
-        return enrichments
+        return {word: enrichments}
 
     def __call__(self, doc: Doc) -> Doc:
         """"""
+        words = set()
         for token in doc:
             if token.is_punct or token.is_stop or token.ent_type != 0:
                 continue
-            enrichments = self._make_request(token.lemma_)
-            token._.concepts = enrichments
-            doc._.concepts = enrichments
+            words.add(token.lemma_)
+        responses = self._make_requests(list(words))
+        enrichments = dict(ChainMap(*map(self._parse_response, responses)))
+
+        for token in doc:
+            if token.is_punct or token.is_stop or token.ent_type != 0:
+                continue
+            token._.concepts = enrichments[token.lemma_]
+
         return doc
 
 
 if __name__ == "__main__":
     import spacy
+    import time
 
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("concepcy")
 
-    import time
     s = time.time()
     doc = nlp(
-        """The Jan. 6 select committee’s latest public hearing went inside the White House to detail then-President Donald Trump’s hourslong refusal to call for an end to the Capitol riot. The hearing marked the final scheduled presentation of the committee’s initial findings from its investigation of the Jan. 6, 2021, insurrection until September."""
+        """The Jan. 6 select committee’s latest public hearing went inside the White House to detail then-President 
+        Donald Trump’s hourslong refusal to call for an end to the Capitol riot. The hearing marked the final scheduled 
+        presentation of the committee’s initial findings from its investigation of the Jan. 6, 2021, insurrection until 
+        September."""
     )
     print(time.time() - s)
+
+    for token in doc:
+        print(token._.concepts)
